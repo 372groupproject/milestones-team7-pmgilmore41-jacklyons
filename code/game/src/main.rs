f4f378f8698e32f2f235a9bfa1668585d02c9954
pmgilmore41 <31549041+pmgilmore41@users.noreply.github.com>
2020-04-19 22:25:24 -0700

    Game update
    Game playing started

--- code/game/src/main.rs
+++ code/game/src/main.rs
@@ -1,11 +1,30 @@
+use std::io;
+use std::panic;
 mod lib;
 
 fn main() {
     let mut board = lib::connect4::Connect4Board::new();
-    board.drop(1,1);
-    board.drop(1,1);
-    board.drop(1,1);
-    board.drop(1,1);
-    board.display();
+    
+    let mut win = 0;
+    let mut buff = String::new();
+
+    println!("Welcome to Connect 4 in Rust!");
+    println!("Who's playing today? Enter your name below...");
+    io::stdin().read_line(&mut buff).unwrap();
+    let name = buff.trim();
+
+    println!("Alright {}, ready to play? The rules are simple pick a column and drop your \ntoken and the token will fall to the bottom most row not filled.",name);
+    println!("You are '1' and your oppenent is '2' and all empty spots are '0'");
+
+    let mut col: usize = 0;
+    while win != 1 {
+        board.display();
+        buff = String::new();
+        io::stdin().read_line(&mut buff).unwrap();
+
+        col = buff.trim().parse::<usize>().unwrap();
+
+        win = board.drop(1, col);
+    }
     
 }
